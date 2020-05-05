98044b54b5e665749dd61a9c4f55be1d97d7169a
Elliot <elliotmrgn@gmail.com>
2020-05-04 22:48:15 -0400

    file name change

---
+++ routes/apiRoutes.js
@@ -0,0 +1,38 @@
+const db = require('../db/db.json');
+const fs = require('fs');
+
+module.exports = (app) => {
+    app.get('/api/notes', (req, res) => {
+        res.json(db);
+    });
+
+    app.post('/api/notes', (req, res) => {
+        const note = req.body;
+        db.push(note);
+        //writes to database
+        dbWrite();
+        //need a response or the express app will hang.
+        res.send('done!');
+    })
+
+    app.delete('/api/notes/:id', (req, res) => {
+        //index of array to delete
+        let index = req.params.id - 1;
+        db.splice(index, 1);
+        dbWrite();
+        res.send('done!');
+    })
+}
+
+const dbWrite = () => {
+    //updating id's
+    db.forEach((note, i) => {
+        //cant start id at 0 or it cant be viewed
+        note.id = i + 1;
+    });
+
+    fs.writeFile('./db/db.json', JSON.stringify(db), err => {
+        if (err) console.error(err)
+        else console.log("successful rewrite")
+    })
+}
